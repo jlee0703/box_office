252b92796af7327e262b3936693faa86b78d8db8
jlee0703 <jlee0703@hotmail.com>
2020-07-26 15:45:20 +0900

    Updates

---
+++ precache-manifest.6955bd680208523bf8640089dbe14746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354bb8fa7fb0b7d4192342f6f832b39b",
+    "url": "/box_office/index.html"
+  },
+  {
+    "revision": "0ef292465fdb5ba6cd6f",
+    "url": "/box_office/static/css/main.c7af75db.chunk.css"
+  },
+  {
+    "revision": "b6846fa761f704090348",
+    "url": "/box_office/static/js/2.f8f8b1c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/box_office/static/js/2.f8f8b1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef292465fdb5ba6cd6f",
+    "url": "/box_office/static/js/main.ffa2e72f.chunk.js"
+  },
+  {
+    "revision": "f2bd544c65361c5fc342",
+    "url": "/box_office/static/js/runtime-main.5df3aae3.js"
+  }
+]);
